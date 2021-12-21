2951ff20385333fd3f9fbd6fe8d3ab1dc3e94f82
Henrique Guedes <henriquebguedes5@gmail.com>
2021-12-21 16:02:54 -0300

    adicionado algoritmo de cadastro de peças

---
+++ README.txt
@@ -0,0 +1,15 @@
+**3 REQUISITOS**
+
+1) Verificar se a peça possui mais de 100g;
+    Sim: pode cadastrar
+    Não: não cadastrar
+
+2) Verificar número de peças cadastradas - NÃO pode ter mais de 10 peças;
+    Menos de 10: pode cadastrar;
+    10 ou mais: não pode cadastrar;
+
+3) Verificar comprimento do nome da peça;
+    Menos de 3 caracteres: não pode cadastrar;
+    3 ou mais caracteres: pode cadastrar;
+
+    ------------------------x------------------------
---
+++ pecas.js
@@ -0,0 +1,29 @@
+// Variáveis são um espaço na memória do computador para guardar algum valor.
+
+let pesoPeca = 101;
+
+if (pesoPeca > 100) {
+    console.log("Peça cadastrada!");
+} else {
+    console.log("Erro! A peça pesa menos de 100g")
+};
+
+let numeroPecas = 12;
+
+if (numeroPecas < 10) {
+    console.log("Peças cadastrada!");
+} else {
+    console.log("Erro! Limite de peças excedido!")
+};
+
+let nomePeca = "Disco de Freio";
+
+console.log ("O comprimento do nome da peça é:", nomePeca.length);
+
+let tamanhoNome = nomePeca.length;
+
+if (tamanhoNome < 3 ) {
+    console.log("Não é possível cadastrar! Nome muito pequeno");
+} else {
+    console.log("Peça cadastrada!")
+}
